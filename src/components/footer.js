d10773ff27e958ce360dc996cccd9fa350ab9f53
Kyle Lux <kyleslux86@gmail.com>
2022-01-11 05:25:23 -0500

    added footer info

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from "react";  
+
+ 
+function Footer() {
+	return (
+        
+        <div className="footer" id="footer">
+            <h4 className='footer-info p-4 mt-4 mb-0'>Thanks for stopping by!</h4>
+            <p className='footer-info p-2 mb-0'>
+                Kyle Lux  |  <a className= "text-reset" href="https://github.com/Risingphoenix86" target='_blank' rel="noreferrer">GitHub</a> | <img src="https://img.icons8.com/color/30/000000/linkedin.png" alt="LinkedIn" className="icon"/><a className= "text-reset" href="https://www.linkedin.com/in/kyle-lux-/" target='_blank' rel="noreferrer">LinkedIn</a> 
+            </p>
+
+        </div>
+    
+	);
+}
+
+export default Footer;
