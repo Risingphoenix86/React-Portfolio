6dd7033b3b394aee2ddb591f7b57b6f2d2bdc8e1
Kyle Lux <kyleslux86@gmail.com>
2022-01-18 18:26:52 -0500

    push of current state, unable to get working before deadline submitting what I have for consideration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // Importing components
-import NavBar from './components/nav.js';
-import Header from './components/header.js';
-import Portfolio from './components/portfolio.js';
+import NavBar from './components/nav';
+import Header from './components/header';
+import Portfolio from './components/portfolio';
 import Contact from './components/contacts';
-import Footer from './components/footer.js';
+import Footer from './components/footer';
 
 function App() {
+  const [pages] = useState([
+      { name: "about me" },
+      { name: "portfolio" },
+      { name: "contact" },
+      { name: "resume" }
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
-    <div className="App">
-       < NavBar />
-       < Header />
-       < Portfolio /> 
-       < Contact />
-       < Footer /> 
-    </div>
+      <div>
+          <Header>
+              <NavBar
+                  pages={pages}
+                  setCurrentPage={setCurrentPage}
+                  currentPage={currentPage}
+              ></NavBar>
+          </Header>
+          <main>
+              <Page currentPage={currentPage}></Page>
+          </main>
+          <Footer />
+      </div>
   );
 }
 
